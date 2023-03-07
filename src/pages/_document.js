4da9c1954d735f88174079099f9a45698152f385
Kevintjuhz <k.quaedvlieg@outlook.com>
2023-03-07 17:12:09 +0100

    Changed tracking url back

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
                 __html: `! function (e, t, p, r, n, a, s) {
                     e[r] || ((n = e[r] = function () {
                         n.process ? n.process.apply(n, arguments) : n.queue.push(arguments)
-                    }).queue = [], n.t = +new Date, (a = t.createElement(p)).async = 1, a.src = "https://api6.tim.acc02.prepr.dev/js/prepr.min.js?t=" + 864e5 * Math.ceil(new Date / 864e5), (s = t.getElementsByTagName(p)[0]).parentNode.insertBefore(a, s))
+                    }).queue = [], n.t = +new Date, (a = t.createElement(p)).async = 1, a.src = "https://cdn.tracking.prepr.io/js/prepr.min.js?t=" + 864e5 * Math.ceil(new Date / 864e5), (s = t.getElementsByTagName(p)[0]).parentNode.insertBefore(a, s))
                 }(window, document, "script", "prepr"), prepr("init", "179d3d7e5dbcd4feb35fd0e0fed2e6e7fdd4c43543f439eab62a17ac1bea9548"), prepr("event", "pageload");`
             }}
             />
