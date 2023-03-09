23c58ebece99586441f59b00b73229086a9e80d9
kevinquaedvlieg <kevin@kqcreations.com>
2023-03-09 17:15:48 +0100

    Fix type

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -39,7 +39,8 @@ export default function App({ Component, pageProps }) {
     )
 }
 
-App.getServerSideProps = () => [
+App.getServerSideProps = () => {
     // Chameleon user guides
-    chmln.init('SliKIsOn78Fnm9uLXgcFyI5oJNZYwY5dPgAX2LTvAAvCgl-1PAiet-EgsLlncdOAqRVOPh', { fastUrl: 'https://fast.chameleon.io/' })
-]
+    chmln.init('SliKIsOn78Fnm9uLXgcFyI5oJNZYwY5dPgAX2LTvAAvCgl-1PAiet-EgsLlncdOAqRVOPh', { fastUrl: 'https://fast.chameleon.io/' });
+    chmln.identify(1)
+}
