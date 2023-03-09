426a2aef6f88f29d33d077cee990cb81f97243f7
kevinquaedvlieg <kevin@kqcreations.com>
2023-03-09 17:22:25 +0100

    test

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,8 +15,7 @@ export default function Document() {
             />
             <Script id="userguiding-id" dangerouslySetInnerHTML={{
                 __html: "(function(g,u,i,d,e,s){g[e]=g[e]||[];var f=u.getElementsByTagName(i)[0];var k=u.createElement(i);k.async=true;k.src='https://static.userguiding.com/media/user-guiding-'+s+'-embedded.js';f.parentNode.insertBefore(k,f);if(g[d])return;var ug=g[d]={q:[]};ug.c=function(n){return function(){ug.q.push([n,arguments])};};var m=['previewGuide','finishPreview','track','identify','triggerNps','hideChecklist','launchChecklist'];for(var j=0;j<m.length;j+=1){ug[m[j]]=ug.c(m[j]);}})(window,document,'script','userGuiding','userGuidingLayer','395789936ID');"
-            }}
-            />
+            }}/>
         </Head>
       <body className="bg-gray-50">
         <Main />
