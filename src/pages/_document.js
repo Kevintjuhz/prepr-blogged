acb0841dde46faaaec95f33811cbadf4dcf0f58b
kevinquaedvlieg <kevin@kqcreations.com>
2023-03-09 17:20:05 +0100

    Implemented userguiding

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,6 +13,10 @@ export default function Document() {
                 }(window, document, "script", "prepr"), prepr("init", "179d3d7e5dbcd4feb35fd0e0fed2e6e7fdd4c43543f439eab62a17ac1bea9548"), prepr("event", "pageload");`
             }}
             />
+            <Script id="userguiding-id" dangerouslySetInnerHTML={{
+                __html: "(function(g,u,i,d,e,s){g[e]=g[e]||[];var f=u.getElementsByTagName(i)[0];var k=u.createElement(i);k.async=true;k.src='https://static.userguiding.com/media/user-guiding-'+s+'-embedded.js';f.parentNode.insertBefore(k,f);if(g[d])return;var ug=g[d]={q:[]};ug.c=function(n){return function(){ug.q.push([n,arguments])};};var m=['previewGuide','finishPreview','track','identify','triggerNps','hideChecklist','launchChecklist'];for(var j=0;j<m.length;j+=1){ug[m[j]]=ug.c(m[j]);}})(window,document,'script','userGuiding','userGuidingLayer','395789936ID');"
+            }}
+            />
         </Head>
       <body className="bg-gray-50">
         <Main />
