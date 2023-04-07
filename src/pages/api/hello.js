af684f3b30885e37b106c19e1559740c1ded0221
Kevintjuhz <kevin@kqcreations.com>
2023-04-07 12:03:59 +0200

    Change

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const json = require('../../../response.json')
 
+// Change
 export default function handler(req, res) {
   res.status(200).json(json)
 }
